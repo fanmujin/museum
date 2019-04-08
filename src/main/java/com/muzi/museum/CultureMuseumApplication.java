e163579e252a264d19d58edf3ca19826a7e9d1c0
muzi_pro <muzi_pro@163.com>
2019-04-08 13:03:44 +0800

    配置了jpa

--- src/main/java/com/muzi/museum/CultureMuseumApplication.java
+++ src/main/java/com/muzi/museum/CultureMuseumApplication.java
@@ -1,15 +1,11 @@
 package com.muzi.museum;
 
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import tk.mybatis.spring.annotation.MapperScan;
 
-import java.util.logging.Logger;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.muzi.museum"})
