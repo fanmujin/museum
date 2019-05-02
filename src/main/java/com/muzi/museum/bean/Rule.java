f7a0ccbfe4b12cce45a6cacc4d6f6e8fa6c063e9
muzi_pro <muzi_pro@163.com>
2019-05-02 10:04:05 +0800

    rule实体类

---
+++ src/main/java/com/muzi/museum/bean/Rule.java
@@ -0,0 +1,75 @@
+package com.muzi.museum.bean;
+
+import java.util.Date;
+
+public class Rule {
+    private Integer id;
+
+    private String name;
+
+    private String picture;
+
+    private String typee;
+
+    private Date ptime;
+
+    private Date createtime;
+
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture == null ? null : picture.trim();
+    }
+
+    public String getTypee() {
+        return typee;
+    }
+
+    public void setTypee(String typee) {
+        this.typee = typee == null ? null : typee.trim();
+    }
+
+    public Date getPtime() {
+        return ptime;
+    }
+
+    public void setPtime(Date ptime) {
+        this.ptime = ptime;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+}
