a531f17ce6fe8de5adc3fc5eb51744167eac6ba4
muzi_pro <muzi_pro@163.com>
2019-05-07 17:32:01 +0800

    完成展览所有接口

--- src/main/java/com/muzi/museum/web/DisplayController.java
+++ src/main/java/com/muzi/museum/web/DisplayController.java
@@ -7,10 +7,10 @@ import com.muzi.museum.service.IDisplayService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -23,22 +23,55 @@ public class DisplayController {
     //查询所有的展览信息
     @GetMapping("findAllDisplay")
     @ApiOperation(value = "通过id查询相关网站")
-    public Result findAllDisplay(){
+    public Result findAllDisplay() {
         List<Display> list = iDisplayService.findAllDisplay();
         return Result.success(list);
     }
+
     //通过id查询展览信息
-    @GetMapping("findDisplayById/{id}")
+    @GetMapping("findDisplayById")
     @ApiOperation(value = "查询displaycha查询展览信息")
-    public Result findDisplayById(int id){
+    public Result findDisplayById(@RequestParam("id") int id) {
         Display display = iDisplayService.findDisplayById(id);
         return Result.success(display);
     }
+
     //通过Display的id查询下面的图片信息
     @GetMapping("findDisplayPictureById/{id}")
     @ApiOperation(value = "查询display的图片信息")
-    public DisplayVM findDisplayPictureById(int id){
+    public DisplayVM findDisplayPictureById(int id) {
         DisplayVM displayVM = iDisplayService.finDisplayPictureById(id);
         return displayVM;
+    }
+
+    //添加新的展览信息
+    @PostMapping("addDisplay")
+    @ApiOperation(value = "添加新的display")
+    public String addDisplay(Display display,
+                             @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @RequestParam Date start,
+                             @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @RequestParam Date end) {
+        display.setDiapalyStartdate(start);
+        display.setDisplayEnddate(end);
+        int re = iDisplayService.addDisplay(display);
+        return re > 0 ? "SUCCESS" : "ERROR";
+    }
+
+    //更新展览信息
+    @PostMapping("updateDisplay")
+    @ApiOperation(value = "更新展栏消息")
+    public String updateDisplay(Display display,
+                                @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @RequestParam Date start,
+                                @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @RequestParam Date end) {
+        display.setDiapalyStartdate(start);
+        display.setDisplayEnddate(end);
+        int re = iDisplayService.updateByPrimaryKey(display);
+        return re > 0 ? "SUCCESS" : "ERROR";
+    }
+    //删除展览信息
+    @DeleteMapping("deleteById")
+    @ApiOperation(value = "删除展览信息")
+    public String deleteById(@RequestParam ("id") int id){
+        int re = iDisplayService.deleteDisplay(id);
+        return re > 0 ? "SUCCESS" : "ERROR";
     }
 }
